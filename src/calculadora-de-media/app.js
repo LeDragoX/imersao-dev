a6bc99cce6fdd2610512e3b8c0f1f9ac50b95a0d
Plínio Larrubia <plinio2xd@gmail.com>
2021-09-17 20:15:16 -0300

    Aula 1 e 2 Fixes

--- src/calculadora-de-media/app.js
+++ src/calculadora-de-media/app.js
@@ -1,13 +1,18 @@
 let nome = "Plínio";
-let grades = [9, 7, 4, 2];
+let grades = [
+  parseFloat(Math.random() * 10),
+  parseFloat(Math.random() * 10),
+  parseFloat(Math.random() * 10),
+  parseFloat(Math.random() * 10),
+];
 let finalGrade = 0;
 
 grades.map((g) => {
-  document.getElementById("page-grades").innerHTML += " | " + g + " | ";
+  document.getElementById("page-grades").innerHTML += " | " + g.toFixed(1) + " | ";
   finalGrade += g;
 });
 
-finalGrade = (finalGrade / grades.length).toFixed(2);
+finalGrade = (finalGrade / grades.length).toFixed(1);
 
 console.log("Bem vindo:", nome, finalGrade);
 document.getElementById("page-final-grade").innerHTML += " " + finalGrade;
